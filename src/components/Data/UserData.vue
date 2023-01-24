1d6b3540a9f501789ffe7f598e372798423f2b04
Rikai-TungVu <tung.vu@rikai.technology>
2023-01-24 10:56:06 +0900

    config by 2 ways, ref or props

--- src/components/Data/UserData.vue
+++ src/components/Data/UserData.vue
@@ -4,20 +4,29 @@
 </template>
 
 <script>
-import { computed, ref } from 'vue';
+import { computed } from 'vue';
+// (2) Not necessary to import ref when use 'props' way
 
 export default {
   props: ['firstName', 'lastName', 'age'],
   //Define props in UserData(child)
 
   //Composition API
-  setup() {
-    const firstName = ref('');
-    const lastName = ref('');
+  setup(props) {
+    // (2) use 'props' to transfer data
+    // (1)Traditions Ways of Compositions API
+    // const firstName = ref('');
+    // const lastName = ref('');
+    // define inside Compositons API
+
+    // (1)Traditions Ways of Compositions API
+    // const uName = computed(function () {
+    //   return firstName.value + ' ' + lastName.value;
+    // });
 
     const uName = computed(function () {
-      return firstName.value + ' ' + lastName.value;
-    }); //
+      return props.firstName + ' ' + props.lastName;
+    }); // (2) Use 'props' ways
 
     return {
       uName,
