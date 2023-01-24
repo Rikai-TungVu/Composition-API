9e818f9bdb547de26d22442a2a2788a712ea7224
Rikai-TungVu <tung.vu@rikai.technology>
2023-01-24 10:11:06 +0900

    devide css file and define component and props

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
-    <h3>{{ age }}</h3>
+    <user-data></user-data>
+    <!-- call UserData(child) -->
     <button @click="setAge">Change Age</button>
     <div>
       <input type="text" placeholder="First Name" v-model="firstName" />
@@ -15,12 +15,19 @@
 </template>
 
 <script>
+import './components/graphic/setting.css';
+
+import UserData from './components/Data/UserData.vue';
 import { ref, computed, watch } from 'vue';
 /* import { ref } from 'vue'; */
 // ref work with single data(.value)
 // reactive work with group data
 
 export default {
+  components: {
+    UserData,
+    // Define componemt in App.vue(parent)
+  },
   setup() {
     // const uName = ref('Monday');
     const firstName = ref('');
@@ -81,26 +88,3 @@ export default {
   // },
 };
 </script>
-
-<style>
-* {
-  box-sizing: border-box;
-}
-
-html {
-  font-family: sans-serif;
-}
-
-body {
-  margin: 0;
-}
-
-.container {
-  margin: 3rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-  text-align: center;
-}
-</style>
---
+++ src/components/Data/UserData.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>{{ userName }}</h2>
+  <h3>{{ age }}</h3>
+</template>
+
+<script>
+export default {
+  props: ['userName', 'age'],
+  //Define props in UserData(child)
+};
+</script>
